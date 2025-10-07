9ae06393fe4adf9161193fab0010ffa9fb1f863a
Aapng-cmd <82652158+Aapng-cmd@users.noreply.github.com>
2025-10-07 22:02:59 +0300

    [TASK-1] (#11)
    * [TASK-1] Task itself.
    
    * [TASK-1] Changed what you wanted
    
    * [TASK-1] Linter, ofcourse

---
+++ artemiy.vernikovskiy/task-1/cmd/service/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b int
+
+	_, err := fmt.Scan(&a)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scan(&b)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	var oper string
+	_, err = fmt.Scan(&oper)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	switch oper {
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(a / b)
+		}
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
