f256b39115e1d9bf9376d95a3e89a9437ea75444
mipxe <mariamozegova8726@gmail.com>
2025-10-10 00:07:28 +0300

    [TASK-1] (#4)
    * [TASK-1] Initial commit
    
    * [TASK-1] Calculator implementation
    
    * [TASK-1] fix input
    
    * [TASK-1] fix mistakes
    
    * [TASK-1] fix errors
    
    * [TASK-1] Delete return
    
    * [TASK-1] fix error description

---
+++ maria.mozhegova/task-1/cmd/service/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		op1, op2  int
+		operation string
+	)
+	_, err := fmt.Scan(&op1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&op2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, err = fmt.Scan(&operation)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	switch operation {
+	case "+":
+		fmt.Println(op1 + op2)
+	case "-":
+		fmt.Println(op1 - op2)
+	case "*":
+		fmt.Println(op1 * op2)
+	case "/":
+		if op2 == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(op1 / op2)
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
---
+++ maria.mozhegova/task-1/go.mod
@@ -0,0 +1,3 @@
+module github.com/mipxe/task-1
+
+go 1.22.7
