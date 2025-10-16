363654b63100e88ef54cd923e4bcb559f926d27d
sharksonk <sharksonk@gmail.com>
2025-10-17 00:41:47 +0300

    [TASK-1] (#46)
    * [TASK-1] initial commit for task-1
    
    * [TASK-1] initial commit for task-1
    
    * [TASK-1] add go.mod
    
    * [TASK-1] add realisation

---
+++ sofia.sharifullina/task-1/cmd/service/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		a, b int
+		op   string
+	)
+	_, err := fmt.Scan(&a)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&b)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, err = fmt.Scan(&op)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+	switch op {
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+	case "/":
+		if b != 0 {
+			fmt.Println(a / b)
+			return
+		}
+		fmt.Println("Division by zero")
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
---
+++ sofia.sharifullina/task-1/go.mod
@@ -0,0 +1,3 @@
+module github.com/sharksonk/task-1
+
+go 1.22.7
