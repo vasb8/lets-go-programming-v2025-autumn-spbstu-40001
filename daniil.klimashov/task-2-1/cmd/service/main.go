26de252fd41dd34b808144da2bd86638c84ef3ea
vasb8 <yavasbobr@gmail.com>
2025-10-20 03:32:13 +0300

    [TASK-2-1] trying to rewrite code structure and change calculating function

--- daniil.klimashov/task-2-1/cmd/service/main.go
+++ daniil.klimashov/task-2-1/cmd/service/main.go
@@ -12,14 +12,7 @@ type department struct {
 	maxTemp int
 }
 
-func newDepartment() *department {
-	return &department{
-		minTemp: defaultMinTemperature,
-		maxTemp: defaultMaxTemperature,
-	}
-}
-
-func (d *department) updateDesiredTemperature(op string, temp int) {
+func (d *department) updateAndGetDesiredTemperature(op string, temp int) {
 	switch op {
 	case ">=":
 		if temp > d.minTemp {
@@ -29,22 +22,21 @@ func (d *department) updateDesiredTemperature(op string, temp int) {
 		if temp < d.maxTemp {
 			d.maxTemp = temp
 		}
-	}
-}
+	default:
+		fmt.Println("Wrong operation")
 
-func (d *department) getDesiredTemperature() int {
-	if d.minTemp <= d.maxTemp {
-		return d.minTemp
+		return
 	}
 
-	return -1
+	if d.minTemp > d.maxTemp {
+		fmt.Println(-1)
+	} else {
+		fmt.Println(d.minTemp)
+	}
 }
 
 func main() {
-	var (
-		departmentAmount int
-		employeeAmount   int
-	)
+	var departmentAmount int
 
 	if _, err := fmt.Scan(&departmentAmount); err != nil {
 		fmt.Println("Invalid number or departments")
@@ -52,40 +44,37 @@ func main() {
 		return
 	}
 
-	if _, err := fmt.Scan(&employeeAmount); err != nil {
-		fmt.Println("Invalid number of employees")
-
-		return
-	}
-
 	for range departmentAmount {
-		dept := newDepartment()
+		var (
+			employeeAmount int
+			temperature    = department{15, 30}
+		)
+
+		if _, err := fmt.Scan(&employeeAmount); err != nil {
+			fmt.Println("Invalid number of employees")
+
+			return
+		}
 
 		for range employeeAmount {
-			var operator string
+			var (
+				operator string
+				temp     int
+			)
+
 			if _, err := fmt.Scan(&operator); err != nil {
 				fmt.Println("Invalid operator")
 
 				return
 			}
 
-			var temp int
 			if _, err := fmt.Scan(&temp); err != nil {
 				fmt.Println("Invalid temperature limit value")
 
 				return
 			}
 
-			dept.updateDesiredTemperature(operator, temp)
-
-			desiredTemp := dept.getDesiredTemperature()
-			fmt.Println(desiredTemp)
-		}
-
-		if _, err := fmt.Scan(&employeeAmount); err != nil {
-			fmt.Println("Invalid number of employees")
-
-			return
+			temperature.updateAndGetDesiredTemperature(operator, temp)
 		}
 	}
 }
